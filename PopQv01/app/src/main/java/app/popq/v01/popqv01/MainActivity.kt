f88df847ce7ca205090b06ea48f7e1cca2b5d680
ashishdan <ashish.awasthi@gmail.com>
2018-07-02 22:18:53 +0800

    refact: handled code convention warnings

--- PopQv01/app/src/main/java/app/popq/v01/popqv01/MainActivity.kt
+++ PopQv01/app/src/main/java/app/popq/v01/popqv01/MainActivity.kt
@@ -14,13 +14,14 @@ import android.widget.TextView
 import android.widget.ArrayAdapter
 import androidx.core.app.ActivityCompat
 import android.content.Intent
-
-
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback, AdapterView.OnItemClickListener {
     private val _tag = "ChooserActivity"
     private val _classes = arrayOf<Class<*>>(LivePreviewActivity::class.java, StillImageActivity::class.java)
-    private val _description_ids = intArrayOf(R.string.desc_camera_source_activity, R.string.desc_still_image_activity)
+    private val _descriptionIds = intArrayOf(R.string.desc_camera_source_activity, R.string.desc_still_image_activity)
+    private val _permissionRequests = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,10 +31,13 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
 
         val listView = findViewById<ListView>(R.id.main_activity_list)
         val adapter = MyArrayAdapter(this, android.R.layout.simple_list_item_2, _classes)
-        adapter.setDescriptionIds(_description_ids)
+        adapter.setDescriptionIds(_descriptionIds)
         listView.adapter = adapter
         listView.setOnItemClickListener(this)
 
+        if (!allPermissionsGranted()) {
+            getRuntimePermissions()
+        }
         fab.setOnClickListener { view ->
             Snackbar.make(view, "No Action defined", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
@@ -61,6 +65,55 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
         startActivity(Intent(this, clicked))
     }
 
+
+    private fun getRequiredPermissions(): Array<String> {
+        try {
+            val info = this.packageManager
+                    .getPackageInfo(this.packageName, PackageManager.GET_PERMISSIONS)
+            val ps = info.requestedPermissions
+            return if (ps != null && ps.isNotEmpty()) {
+                ps
+            } else {
+                arrayOf<String>()
+            }
+        } catch (e: Exception) {
+            return arrayOf<String>()
+        }
+
+    }
+
+    private fun isPermissionGranted(context: Context, permission: String): Boolean {
+        if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED) {
+            Log.i(_tag, "Permission granted: $permission")
+            return true
+        }
+        Log.i(_tag, "Permission NOT granted: $permission")
+        return false
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun getRuntimePermissions() {
+        val allNeededPermissions = ArrayList<String>()
+        for (permission in getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                allNeededPermissions.add(permission)
+            }
+        }
+
+        if (!allNeededPermissions.isEmpty()) {
+            ActivityCompat.requestPermissions(
+                    this, allNeededPermissions.toTypedArray(), _permissionRequests)
+        }
+    }
+
     private class MyArrayAdapter(context: Context, resource: Int, private val classes: Array<Class<*>>) : ArrayAdapter<Class<*>>(context, resource, classes) {
         private var descriptionIds: IntArray? = null
 
@@ -73,7 +126,7 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
             }
 
             (view!!.findViewById(android.R.id.text1) as TextView).text = classes[position].simpleName
-            (view!!.findViewById(android.R.id.text2) as TextView).setText(descriptionIds!![position])
+            (view.findViewById(android.R.id.text2) as TextView).setText(descriptionIds!![position])
 
             return view
         }
